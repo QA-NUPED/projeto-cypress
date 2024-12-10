d78a4e5e0d8f60949c47351e6f58baf32191558d
Thais Mabelli <thaismabellialves@gmail.com>
2024-12-10 13:01:15 -0300

    alterações

--- cypress.config.js
+++ cypress.config.js
@@ -6,15 +6,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-    reporterOptions: {
-      reportDir: 'cypress/reports',    // Caminho onde o relatório será salvo
-      overwrite: true,                 // Sobrescreve relatórios antigos
-      html: true,                      // Gera apenas o relatório HTML
-      json: false,                     // Desabilita a geração de arquivos JSON
-      // A opção `reportFilename` pode ser usada para garantir que o nome do relatório seja fixo
-      reportFilename: 'relatorio-mochawesome.html',
-      // Adicionando o option `merge: false` para evitar tentativa de mesclagem de JSON
-      merge: false,
-    },
+    
   },
 });
