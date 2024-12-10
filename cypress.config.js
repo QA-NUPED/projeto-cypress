808361e23bdcee0b470c80802123245afeca23b1
Thais Mabelli <thaismabellialves@gmail.com>
2024-12-10 15:12:57 -0300

    alterações config cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,18 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-mochawesome-reporter', // Usando o Mochawesome como reporter
   e2e: {
     setupNodeEvents(on, config) {
+      // Inicializa o plugin do Mochawesome
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-    
+    // Configuração para os relatórios
+    reporterOptions: {
+      reportDir: 'cypress/reports/mochawesome', // Diretório para salvar os relatórios
+      overwrite: true,                        // Não sobrescrever os relatórios
+      html: true,                              // Gerar relatórios em HTML
+      json: true                               // Gerar relatórios em JSON
+    }
   },
 });
